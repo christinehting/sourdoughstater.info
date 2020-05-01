63c5ccaf36a2dcbd2f6498a969cf627f9cc13535
Christine Ting <cting@igrad.com>
2020-05-01 10:42:21 -0700

    fix header on all pages

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ jQuery(function($) {'use strict',
 	//Initiat WOW JS
 	new WOW().init();
 
-	// portfolio filter
+	// recipe filter
 	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.recipe-filters>li>a');
+		var $recipe_selectors = $('.recipe-filters>li>a');
 		var $recipe_sections = $('.recipe-section');
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
+		$recipe_selectors.on('click', function(){
+			$recipe_selectors.removeClass('active');
 			$(this).addClass('active');
 			var selector = $(this).attr('data-filter');
 			$recipe_sections.hide();
