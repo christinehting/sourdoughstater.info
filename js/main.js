18f4d2d3beacc371f611e2c72b81b4086e16b0d8
cht <cting@igrad.com>
2020-04-30 22:36:38 -0700

    adding basic page structures to template

--- js/main.js
+++ js/main.js
@@ -5,18 +5,14 @@ jQuery(function($) {'use strict',
 
 	// portfolio filter
 	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
+		var $portfolio_selectors = $('.recipe-filters>li>a');
+		var $recipe_sections = $('.recipe-section');
 		$portfolio_selectors.on('click', function(){
 			$portfolio_selectors.removeClass('active');
 			$(this).addClass('active');
 			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
+			$recipe_sections.hide();
+			$(selector).show();
 			return false;
 		});
 	});
